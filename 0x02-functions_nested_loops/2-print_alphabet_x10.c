7f4adde3e3deb58b3421783f9055720a2058eadd
Amira Esmail <99510485+amiraesmail12@users.noreply.github.com>
2023-06-23 19:19:40 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int count;
+	int count = 0;
 	char letter;
 
-	for (count = 0; count <= 9; count++)
+	while (count++ <= 9)
 	{
-
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-
 		_putchar('\n');
 	}
+}
