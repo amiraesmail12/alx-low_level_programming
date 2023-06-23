f9cdaca4844ad89312adff95d4a96356c8785218
Amira Esmail <amiraesmail777@gmail.com>
2023-06-23 09:01:28 -0700

    rewrite by nested for

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int count;
+	char letter;
 
-	for (i = 0; i <= 10; i++)
-		print_alphabet();
+	for (count = 0; count <= 10; count++)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 
 }
