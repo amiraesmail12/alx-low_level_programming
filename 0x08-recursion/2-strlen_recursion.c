d5f2569a7fe0a7300efcb26f791d62326cd9ae86
Amira Esmail <amiraesmail777@gmail.com>
2023-09-28 18:11:25 -0700

    parentheses problem solved

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,5 @@
 /**
- * strlen_recursion - return length of string
+ * _strlen_recursion - return length of string
  * @s: string to calculate the length of
  *
  * Return: length of the string.
@@ -7,9 +7,7 @@
 #include "main.h"
 int _strlen_recursion(char *s)
 {
-if (*s == '\n')
-{
-	return 0;
-}
+if (*s == '\0')
+	return (0);
 return (1 + _strlen_recursion(s + 1));
 }
