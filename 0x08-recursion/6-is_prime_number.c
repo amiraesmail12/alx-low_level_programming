c62421e29ed68f783a4fca7285960a9800839469
Amira Esmail <amiraesmail777@gmail.com>
2023-10-01 13:10:10 -0700

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+int act_prime(int n, int i);
+/**
+ * is_prime_number - check if the integer is a prime or not.
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime, 0 if not
+ */
+#include "main.h"
+int is_prime_number(int n)
+{
+if (n <= 0)
+	return (n, n - 1);
+return ();
+}
+/**
+ * act_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int act_prime(int n, int i)
+{
+if (i == 1)
+	return (1);
+if (n % i == 0 && i > 0)
+	return (0);
+return (act_prime(n, i - 1));
+}
