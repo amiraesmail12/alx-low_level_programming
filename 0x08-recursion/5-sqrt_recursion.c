7c6db2755b7b67917b165da9af55970fd6877a44
Amira Esmail <amiraesmail777@gmail.com>
2023-10-01 10:30:54 -0700

    solve it with another function because loops are not allowed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,15 +7,23 @@
 #include "main.h"
 int _sqrt_recursion(int n)
 {
-int i = 0;
 if (n < 0)
 	return (-1);
-for (i = 0; i < n; i++)
-{
-	if (i * i > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
+return (act_sqrt_recursion(n, 0));
 }
-return (_sqrt_recursion(n));
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int act_sqrt_recursion(int n, int i)
+{
+if (i * i > n)
+	return (-1);
+if (i * i == n)
+	return (i);
+return (act_sqrt_recursion(n, i + 1));
 }
