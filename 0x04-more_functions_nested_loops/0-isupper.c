d7c0093217ed4b5f35a305ec39650764e5943099
Amira Esmail <amiraesmail777@gmail.com>
2023-10-03 15:35:04 -0700

    main.h changed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
-#include "main.h"
 /**
  * _isupper - uppercase letters
  * @c: character to check
  *
  * Return: 0 or 1
  */
+#include "main.h"
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
