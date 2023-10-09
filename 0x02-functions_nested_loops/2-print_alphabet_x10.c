18e26eb166a9dab44b8221adef5631448f7bc12d
Amira Esmail <amiraesmail777@gmail.com>
2023-10-09 16:56:55 -0700

    requerment proplem soved

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_x10
- * Decription:
- * prints 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
