9a86d18cb7dfa370806d93ee4be920ca83f2741b
Amira Esmail <amiraesmail777@gmail.com>
2023-10-10 16:32:03 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints its name follwed by a new line
+ * @argc: argumen t count
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+}
