4c16e832f20f5eb37112297681093804e084bdaf
Amira Esmail <amiraesmail777@gmail.com>
2023-10-10 19:29:42 -0700

    task 1 & 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+typedef struct dog dog_t
 #endif /*_DOG_H_*/
