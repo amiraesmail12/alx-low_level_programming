525fb98dcefa79aa13dcd5a094318da2d375a1fb
Amira Esmail <amiraesmail777@gmail.com>
2023-10-10 17:34:01 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+#include "main.h"
+int main(void)
+{
+_putchar("%s\n", __FILE__);
+return (0);
+}
