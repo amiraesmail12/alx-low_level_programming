e0cf8a4d948ca59431699b534e73e70e7f00f80f
Amira Esmail <amiraesmail777@gmail.com>
2023-10-11 17:19:20 -0700

    header file name changed from main.h to function_pointers.h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
+#include "function_pointers.h"
 /**
  * print_name - prints a name.
  * @name: apointer to array of names
